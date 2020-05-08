0bc83efcd155936cc3ed76f738edfd19f632e953
Hail91 <aarongillies88@gmail.com>
2020-05-08 14:29:00 -0400

    added 4th solution

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,19 @@
-# def intersection(arrays):
-#     storage = dict()
-#     results = []
-#     count = 0
-#     for index, arr in enumerate(arrays):
-#         for v in arr:
-#             if storage.get(v) is not None and index > 0:
-#                 storage[v] += 1
-#             elif
-
-
-
-
-    # return result
+def intersection(arrays):
+    storage = dict()
+    results = []
+    count = 0
+    for index, arr in enumerate(arrays):
+        for v in arr:
+            if storage.get(v) is not None and index > 0:
+                storage[v] += 1
+            elif storage.get(v) is None and index == 0:
+                storage[v] = 1
+            else:
+                continue
+    for values in storage:
+        if storage[values] == len(arrays):
+            results.append(values)
+    return results
 
 intersection([[1, 3, 4], [1, 5, 7], [1, 2, 8]])
             #  x->x->x 
